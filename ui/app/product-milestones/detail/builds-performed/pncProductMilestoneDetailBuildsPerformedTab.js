11b29d057faa6d7812f2f705ca507f106f600cca
Dennis Zhou <tozhou@redhat.com>
2022-05-11 13:46:01 +0100

    [NCL-7057] Fix Single quote warning

--- ui/app/product-milestones/detail/builds-performed/pncProductMilestoneDetailBuildsPerformedTab.js
+++ ui/app/product-milestones/detail/builds-performed/pncProductMilestoneDetailBuildsPerformedTab.js
@@ -3,30 +3,30 @@
  * Copyright 2014-2020 Red Hat, Inc., and individual contributors
  * as indicated by the @author tags.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the 'License');
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an 'AS IS' BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 (function () {
-  "use strict";
+  'use strict';
 
   angular
-    .module("pnc.product-milestones")
-    .component("pncProductMilestoneDetailBuildsPerformedTab", {
+    .module('pnc.product-milestones')
+    .component('pncProductMilestoneDetailBuildsPerformedTab', {
       bindings: {
-        productMilestone: "<",
-        performedBuilds: "<",
+        productMilestone: '<',
+        performedBuilds: '<',
       },
       templateUrl:
-        "product-milestones/detail/builds-performed/pnc-product-milestone-detail-builds-performed-tab.html",
+        'product-milestones/detail/builds-performed/pnc-product-milestone-detail-builds-performed-tab.html',
       controller: [Controller],
     });
 
--- ui/app/product-milestones/detail/details/pncProductMilestoneDetailDetailsTab.js
+++ ui/app/product-milestones/detail/details/pncProductMilestoneDetailDetailsTab.js
@@ -3,30 +3,30 @@
  * Copyright 2014-2020 Red Hat, Inc., and individual contributors
  * as indicated by the @author tags.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the 'License');
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an 'AS IS' BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 (function () {
-  "use strict";
+  'use strict';
 
   angular
-    .module("pnc.product-milestones")
-    .component("pncProductMilestoneDetailDetailsTab", {
+    .module('pnc.product-milestones')
+    .component('pncProductMilestoneDetailDetailsTab', {
       bindings: {
-        productMilestone: "<",
-        closeResults: "<",
+        productMilestone: '<',
+        closeResults: '<',
       },
       templateUrl:
-        "product-milestones/detail/details/pnc-product-milestone-detail-details-page.html",
+        'product-milestones/detail/details/pnc-product-milestone-detail-details-tab.html',
       controller: [Controller],
     });
 
@@ -38,7 +38,7 @@
     // --------------------
 
     $ctrl.$onInit = () => {
-      $ctrl.closeStatus = $ctrl.productMilestone.endDate ? "CLOSED" : "OPEN";
+      $ctrl.closeStatus = $ctrl.productMilestone.endDate ? 'CLOSED' : 'OPEN';
       $ctrl.latestCloseResult = $ctrl.closeResults.data[0];
     };
   }
