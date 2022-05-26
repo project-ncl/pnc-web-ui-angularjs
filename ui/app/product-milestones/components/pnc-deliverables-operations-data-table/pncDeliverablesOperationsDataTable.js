5a8d1e3dae7f6be1d419bf4eea5a36a387437c1c
Dennis Zhou <tozhou@redhat.com>
2022-05-26 02:11:27 +0100

    [NCL-7042] Deliverable Operations list and details page

---
+++ ui/app/product-milestones/components/pnc-deliverables-operations-data-table/pncDeliverablesOperationsDataTable.js
@@ -0,0 +1,92 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014-2020 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+(function () {
+  "use strict";
+
+  angular
+    .module("pnc.product-milestones")
+    .component("pncDeliverablesOperationsDataTable", {
+      bindings: {
+        /**
+         * Array: The list of deliverables operations to display in the table
+         */
+        deliverablesOperations: "<",
+      },
+      templateUrl:
+        "product-milestones/components/pnc-deliverables-operations-data-table/pnc-deliverables-operations-data-table.html",
+      controller: ["filteringPaginator", "SortHelper", Controller],
+    });
+
+  function Controller(filteringPaginator, SortHelper) {
+    const $ctrl = this;
+
+    const PAGE_NAME = "deliverablesOperationsDataTable";
+
+    // -- Controller API --
+
+    $ctrl.filteringFields = [
+      {
+        id: "id",
+        title: "Id",
+        placeholder: "string | !string | s?ring | st*ng",
+        filterType: "text",
+      },
+      {
+        id: "progressStatus",
+        title: "Status",
+        placeholder: "Filter by Status",
+        filterType: "select",
+        filterValues: ["NEW", "IN_PROGRESS", "FINISHED"],
+      },
+      {
+        id: "result",
+        title: "Result",
+        placeholder: "Filter by Result",
+        filterType: "select",
+        filterValues: ["SUCCESSFUL", "FAILED", "SYSTEM_ERROR"],
+      },
+    ];
+
+    $ctrl.sortingFields = [
+      {
+        id: "id",
+        title: "Id",
+      },
+      {
+        id: "progressStatus",
+        title: "Status",
+      },
+      {
+        id: "result",
+        title: "Result",
+      },
+      {
+        id: "submitTime",
+        title: "Submit Time",
+      },
+    ];
+
+    // --------------------
+
+    $ctrl.$onInit = () => {
+      $ctrl.filteringPage = filteringPaginator($ctrl.deliverablesOperations);
+
+      $ctrl.sortingConfigs = SortHelper.getSortConfig(PAGE_NAME);
+    };
+  }
+})();
