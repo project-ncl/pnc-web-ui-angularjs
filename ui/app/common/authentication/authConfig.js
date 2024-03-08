754e834c82334a07e89e7d99dbc4ec6059e32190
Jozef Mrazek <jmrazek@redhat.com>
2024-03-08 13:36:25 +0100

    [NCL-8479] Fix keycloak issues

--- ui/app/common/authentication/authConfig.js
+++ ui/app/common/authentication/authConfig.js
@@ -20,7 +20,7 @@
   
   angular.module('pnc.common.authentication').provider('authConfig',
     function () {
-      var ssoTokenLifespan = 86400000; // Default: 24 Hours
+      var ssoTokenLifespan = 10; // 10 sec - minimal time for token validity, token is now valid for 5 min max (auth.redhat)
   
       function getSsoTokenLifespan() {
         return ssoTokenLifespan;
